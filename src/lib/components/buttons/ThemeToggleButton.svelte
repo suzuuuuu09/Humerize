022ca3d023d613ebf05274cbc8de094a58a508df
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-01-02 15:36:50 +0900

    ✨ darkmode

---
+++ src/lib/components/buttons/ThemeToggleButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+
+  let isDarkMode = false
+  function toggleDarkTheme() {
+    isDarkMode = !isDarkMode
+    document.documentElement.classList.toggle("dark")
+  }
+</script>
+
+<button
+  on:click={toggleDarkTheme}
+  class="p-2 rounded-lg hover:bg-slate-100 dark:hover:bg-slate-700 transition-colors"
+>
+  {#if isDarkMode}
+  <Icon icon="mdi:weather-sunny" />
+  {:else}
+  <Icon icon="mdi:weather-night" />
+  {/if}
+</button>
